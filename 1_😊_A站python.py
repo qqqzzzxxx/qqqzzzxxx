c21de559facbb81a77de37c8b0e6b391ce414731
qqqzzzxxx <96412414+qqqzzzxxx@users.noreply.github.com>
2023-12-01 16:18:50 +0800

    Delete 1_😊_A站python.py

--- 1_😊_A站python.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pymongo
-# 连接到 MongoDB 数据库
-client = pymongo.MongoClient('localhost', 27017)
-db = client.pythondb
-collection = db.PY_data
-# 显示数据
-results = collection.find()
-st.table(results)
